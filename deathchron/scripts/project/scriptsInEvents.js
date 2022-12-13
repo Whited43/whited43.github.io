779bbcf46a3ea58d375072166abee320c1a84878
Whited43 <65544151+Whited43@users.noreply.github.com>
2022-12-13 21:55:52 +0300

    Add files via upload

---
+++ deathchron/scripts/project/scriptsInEvents.js
@@ -0,0 +1,21 @@
+
+
+
+const scriptsInEvents = {
+
+	async Gameplay_Event483_Act1(runtime, localVars)
+	{
+		//ADD GO TO MENU OR STORE OR MAIN MAP
+		
+	},
+
+	async Gameplay_Event485_Act1(runtime, localVars)
+	{
+		//ADD GO TO MENU OR STORE OR MAIN MAP
+		
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
