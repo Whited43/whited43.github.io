1296cb035420b27a0dd88a8474b437657ddf7f6c
Whited43 <65544151+Whited43@users.noreply.github.com>
2022-12-14 23:43:19 +0300

    Add files via upload

---
+++ death2/scripts/project/scriptsInEvents.js
@@ -0,0 +1,21 @@
+
+
+
+const scriptsInEvents = {
+
+	async Gameplay_Event507_Act1(runtime, localVars)
+	{
+		//ADD GO TO MENU OR STORE OR MAIN MAP
+		
+	},
+
+	async Gameplay_Event509_Act1(runtime, localVars)
+	{
+		//ADD GO TO MENU OR STORE OR MAIN MAP
+		
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
