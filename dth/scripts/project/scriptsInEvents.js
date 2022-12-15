0a4e3a7f2ce258f592c35d953e7c76cbb343250a
Whited43 <65544151+Whited43@users.noreply.github.com>
2022-12-15 17:03:52 +0300

    Add files via upload

---
+++ dth/scripts/project/scriptsInEvents.js
@@ -0,0 +1,21 @@
+
+
+
+const scriptsInEvents = {
+
+	async Gameplay_Event517_Act1(runtime, localVars)
+	{
+		//ADD GO TO MENU OR STORE OR MAIN MAP
+		
+	},
+
+	async Gameplay_Event519_Act1(runtime, localVars)
+	{
+		//ADD GO TO MENU OR STORE OR MAIN MAP
+		
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
